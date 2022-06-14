5e3198816846271c4e1a713f5f22d8b9f3ad9846
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2022-06-14 16:16:40 -0300

    Mover constantes e método

--- ConversaoRealDolar/Program.cs
+++ ConversaoRealDolar/Program.cs
@@ -1,30 +1,42 @@
-// Cotação do dólar no dia 3 de maio de 2022.
-double cotacaoDoDolarHoje = 4.97;
-const double VALOR_MINIMO_DE_COMPRA_EM_DOLAR = 100;
-const double VALOR_MAXIMO_DE_COMPRA_EM_DOLAR = 2500;
-double valorConvertidoDaCompra;
-double valorCompraUsuario;
+namespace ConversaoRealDolar;
 
-double converterRealParaDolar(double valorDaCompraEmReais)
+internal class Program
 {
-    valorConvertidoDaCompra =  valorDaCompraEmReais / cotacaoDoDolarHoje;
-    return valorConvertidoDaCompra;
+    private const double _valorMinimo = 100;
+    private const double _valorMaximo = 2500;
+    private const double _cotacaoDoDolarHoje = 4.97;
+
+    private static void Main()
+    {
+        // Cotação do dólar no dia 3 de maio de 2022.
+        double valorConvertidoDaCompra;
+        double valorCompraUsuario;
+
+        Console.WriteLine("Digite o valor em Real da sua compra: ");
+        valorCompraUsuario = Convert.ToDouble(Console.ReadLine());
+
+        var valorConvertido = ConverterRealParaDolar(valorCompraUsuario);
+
+
+        if (valorConvertido < _valorMinimo)
+        {
+            Console.WriteLine("A compra mínima permitida é de USD$ " + _valorMinimo + " e o valor convertido atual de sua compra é de USD$ " + valorConvertido);
+        }
+        else if (valorConvertido > _valorMaximo)
+        {
+            Console.WriteLine("A compra máxima permitida é de USD$ " + _valorMaximo + " e o valor convertido atual de sua compra é de USD$ " + valorConvertido);
+        }
+        else
+        {
+            Console.WriteLine("Você pode comprar USD$ " + valorConvertido);
+        }
+
+
+    }
+
+    private double ConverterRealParaDolar(double valorDaCompraEmReais)
+    {
+        valorConvertidoDaCompra = valorDaCompraEmReais / cotacaoDoDolarHoje;
+        return valorConvertidoDaCompra;
+    }
 }
-
-Console.WriteLine("Digite o valor em Real da sua compra: ");
-valorCompraUsuario = Convert.ToDouble(Console.ReadLine());
-
-var valorConvertido = converterRealParaDolar(valorCompraUsuario);
-
-
-if (valorConvertido < VALOR_MINIMO_DE_COMPRA_EM_DOLAR)
-{
-    Console.WriteLine("A compra mínima permitida é de USD$ " + VALOR_MINIMO_DE_COMPRA_EM_DOLAR + " e o valor convertido atual de sua compra é de USD$ " + valorConvertido);
-} else if (valorConvertido > VALOR_MAXIMO_DE_COMPRA_EM_DOLAR)
-{
-    Console.WriteLine("A compra máxima permitida é de USD$ " + VALOR_MAXIMO_DE_COMPRA_EM_DOLAR + " e o valor convertido atual de sua compra é de USD$ " + valorConvertido);
-} else
-{
-    Console.WriteLine("Você pode comprar USD$ " + valorConvertido);
-}
-
